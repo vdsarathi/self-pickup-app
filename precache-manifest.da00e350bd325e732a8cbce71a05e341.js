bfa3a4cbc8eb9661123a6cb041c79fe5433ac8dc
Deepak Sarathi <vdsarathi@gmail.com>
2020-09-02 00:51:52 +0530

    Updates

---
+++ precache-manifest.da00e350bd325e732a8cbce71a05e341.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467a2fa076dfbc4b7cc55a04017039a1",
+    "url": "/self-pickup-app/index.html"
+  },
+  {
+    "revision": "dcc095a1cc800426c2b6",
+    "url": "/self-pickup-app/static/css/2.87d51063.chunk.css"
+  },
+  {
+    "revision": "4c3c11c9b38ef40dadc3",
+    "url": "/self-pickup-app/static/css/main.86364332.chunk.css"
+  },
+  {
+    "revision": "dcc095a1cc800426c2b6",
+    "url": "/self-pickup-app/static/js/2.a5c23f2f.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/self-pickup-app/static/js/2.a5c23f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3c11c9b38ef40dadc3",
+    "url": "/self-pickup-app/static/js/main.1193cf07.chunk.js"
+  },
+  {
+    "revision": "f7e7bfa6dfa9437abf1e",
+    "url": "/self-pickup-app/static/js/runtime-main.f7fae40d.js"
+  },
+  {
+    "revision": "9c12cd3b945e33ee64ae9b1761153d68",
+    "url": "/self-pickup-app/static/media/MyriadPro-Bold.9c12cd3b.otf"
+  },
+  {
+    "revision": "a89cf050c859a23e4b6d9cf5989acf9e",
+    "url": "/self-pickup-app/static/media/MyriadPro-Regular.a89cf050.otf"
+  },
+  {
+    "revision": "4d63236ce9f9a7c10f4ccb4991c57016",
+    "url": "/self-pickup-app/static/media/MyriadPro-Semibold.4d63236c.otf"
+  },
+  {
+    "revision": "ebce79abc7933388665e01991e67740f",
+    "url": "/self-pickup-app/static/media/background.ebce79ab.jpg"
+  },
+  {
+    "revision": "4703acee4e98b265f8bc6ee4db1d7836",
+    "url": "/self-pickup-app/static/media/barcode.4703acee.jpg"
+  },
+  {
+    "revision": "4a82c150c1fc1186702ee058854746e2",
+    "url": "/self-pickup-app/static/media/delivery.4a82c150.png"
+  },
+  {
+    "revision": "a7cfec92ca5f2802257133e24f23ea66",
+    "url": "/self-pickup-app/static/media/login_background.a7cfec92.jpg"
+  },
+  {
+    "revision": "2d6afc0f5c692f5ad286ed25788a8cf9",
+    "url": "/self-pickup-app/static/media/login_background_mobile.2d6afc0f.jpg"
+  },
+  {
+    "revision": "e659cb6f5015124762a923bf36dded99",
+    "url": "/self-pickup-app/static/media/self-pickup.e659cb6f.png"
+  }
+]);
